c2c8e4b2e575d159fb28701b1d5bbe8295f24170
Lukasz Zajaczkowski <zreigz@gmail.com>
2023-03-03 10:39:57 +0100

    init

---
+++ apis/acme/v1alpha1/acme_types.go
@@ -0,0 +1,22 @@
+package v1alpha1
+
+import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+func init() {
+	SchemeBuilder.Register(&Acme{}, &AcmeList{})
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Cluster
+type Acme struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+type AcmeList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Acme `json:"items"`
+}
